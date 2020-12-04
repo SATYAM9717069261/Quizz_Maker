e22c9ad11d12af61d75ab22f86d33c8c543f05c4
Satyam <satyam.mandal.77@gmail.com>
2020-12-04 22:53:37 +0530

    Fix horizontal layout Problrm in Quizz Option Generation

--- Python_FIle/sheet_design.py
+++ Python_FIle/sheet_design.py
@@ -1,50 +1,58 @@
 import sys
 from PySide2 import QtCore
 from PySide2.QtUiTools import QUiLoader
-from PySide2.QtWidgets import QApplication,QVBoxLayout
+from PySide2.QtWidgets import QApplication,QVBoxLayout,QRadioButton,QHBoxLayout
 from PySide2.QtCore import QFile, QCoreApplication, QIODevice
+from Setup import Setup_urls
 
-
-class Setup_urls:
+class Quizz(Setup_urls):
     def __init__(self,url):
-        self.ui_file_name = url
-        self.ui_file = QFile(self.ui_file_name)
+        super(Quizz,self).__init__(url) 
+        self.quizz_option=QHBoxLayout()    # for vertcal use QVBoxLayout for horizontal QHBoxLayout
+        
 
-        if not self.ui_file.open(QIODevice.ReadOnly):                                                         # Error Occure if file not open
-            print("Cannot open {}: {}".format(self.ui_file_name, ui_file.errorString()))
-            sys.exit(-1)
+    def option_add(self):
+        #print(self.window.option_text.displayText())
+        __data__=self.window.option_text.displayText()
+        if(__data__):
+           # print(dir(self.window.Quizz_scroll.layout))
+            # generate Options
+            self.quizz_option.addWidget(QRadioButton(__data__))
+            self.window.Quizz_scroll.setWidget(self.window.scrollAreaWidgetContents.setLayout(self.quizz_option))
+            #self.window.Quizz_Option_frame.layout.addItem( QRadioButton(__data__)  )
+            # end Generate Options
+        else:
+            print("Can't ADD ")
 
-        self.window=QUiLoader().load(self.ui_file)
-        self.ui_file.close()
 
-    def Button_clicks(self):   # all Button click define here And call Further function
-        self.vbox=QVBoxLayout()
+
+class Main_window(Setup_urls):
+    def __init__(self,url):
+        super(Main_window,self).__init__(url)
+        
+    def Button_Click(self):
+        self.vbox=QVBoxLayout() 
         self.window.Add_Quizz.clicked.connect(self.Quizz_UI_ADD)
-        self.window.Add_Fill.clicked.connect(self.Fillup_UI_ADD)
-
-
+        self.window.Add_Fill.clicked.connect(self.Fillup_UI_ADD) 
+    
     def Fillup_UI_ADD(self):
         Fillup=Setup_urls("../UI/Fillup.ui")
-        self.vbox.addWidget(Fillup.window.Fillup)           # never set layout in Source page it generate ISSUES
+        self.vbox.addWidget(Fillup.window.Fillup)
         self.window.Scroll_Area.setWidget(self.window.Scroll_Area_Content.setLayout(self.vbox))
 
     def Quizz_UI_ADD(self):
-        Quizz=Setup_urls("../UI/Quizz.ui")
-        self.vbox.addWidget(Quizz.window.final_quiz)           # never set layout of Source page it generate ISSUES
+        quizz=Quizz("../UI/Quizz.ui")
+        self.vbox.addWidget(quizz.window.final_quiz)
         self.window.Scroll_Area.setWidget(self.window.Scroll_Area_Content.setLayout(self.vbox))
-        #Quizz.window.add_option.clicked.connect(Quizz.Add_option ) 
-        #print(dir(Quizz.window.add_option))
-
-    def Add_option(self):
-        print("Call")
+        quizz.window.add_option.clicked.connect(lambda : quizz.option_add())  # directly can't call
 
 
 
 if __name__ == "__main__":
     QtCore.QCoreApplication.setAttribute(QtCore.Qt.AA_ShareOpenGLContexts)
     app = QApplication(sys.argv)
-    Object1 = Setup_urls("../UI/sheet_design.ui")
-    Object1.Button_clicks()   # all Click event define here
-    Object1.window.show()
+    main=Main_window("../UI/sheet_design.ui")
+    main.Button_Click()
+    main.window.show()
     sys.exit(app.exec_())
 
